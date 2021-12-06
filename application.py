eec343aa58f6cebf44c832656c053decfb0f1f47
pavan485 <76582086+pavan485@users.noreply.github.com>
2021-12-06 18:56:56 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import logger
 import yaml
 import request_handler
-import utils,log
+import utils,log,time
 conf = yaml.safe_load(open('./config/config_catchpoint.yaml'))
 logger = log.get_logger(__name__,conf['log_file'],conf['log_level'])
 class Application(object):
@@ -15,35 +15,37 @@ class Application(object):
         for ndx in range(0, l, n):
             yield iterable[ndx:min(ndx + n, l)]
 
-    def run(self):    
-        try:
-            self.__request_handler.authorize(conf)
-            for test_id_type in conf['test_ids'].values():
-                final_list = []
-                for test_id in self.batch(test_id_type, conf['batch_size']):
-                    conf['test_id_params']=",".join(test_id)
-                    data = self.__request_handler.fetch_data(conf)
-                    expired = self.__request_handler.expired_token_check(data)
+    def run(self):
+        while True:
+            try:
+                self.__request_handler.authorize(conf)
+                for test_id_type in conf['test_ids'].values():
+                    final_list = []
+                    for test_id in self.batch(test_id_type, conf['batch_size']):
+                        conf['test_id_params']=",".join(test_id)
+                        data = self.__request_handler.fetch_data(conf)
+                        expired = self.__request_handler.expired_token_check(data)
 
-                    if expired is True:
-                        self.__request_handler.authorize(conf)
-                        data = self.__request_handler.fetch_data(conf) 
-                    parsed_json = utils.Utils.parse_raw(data)
-                    if final_list is []:
-                        final_list=parsed_json
+                        if expired is True:
+                            self.__request_handler.authorize(conf)
+                            data = self.__request_handler.fetch_data(conf) 
+                        parsed_json = utils.Utils.parse_raw(data)
+                        if final_list is []:
+                            final_list=parsed_json
+                        else:
+                            if parsed_json:
+                                final_list.extend(parsed_json)
+                        logger.info(final_list)
+                    if final_list is not None:
+                        utils.Utils.insert_to_influx(final_list)
                     else:
-                        if parsed_json:
-                            final_list.extend(parsed_json)
-                    logger.info(final_list)
-                if final_list is not None:
-                    utils.Utils.insert_to_influx(final_list)
-                else:
-                    logger.exception('No Data')
-        except Exception as e:
-            logger.exception(str(e))
+                        logger.exception('No Data')
+                time.sleep(900000)
+            except Exception as e:
+                logger.exception(str(e))
+            
      
 if __name__ == '__main__':
     if not utils.Utils.validate_configurations():
         raise RuntimeError('missing configrations')
     Application().run()
-        
